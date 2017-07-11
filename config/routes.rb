40f4c9a766dca3a2ad11c54bd228efa785f182e5
Olivier Wenger <olivier@reivilo.net>
2017-07-11 21:34:18 +0200

    root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "pilots#index"
   resources :flights
   resources :pilots
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
