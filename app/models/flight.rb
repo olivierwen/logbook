f128c93e2159e04ee4842faa43ebb3a6882d599e
Olivier Wenger <olivier@reivilo.net>
2017-07-30 20:55:29 +0200

    Validation and form styling

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,3 +1,4 @@
 class Flight < ApplicationRecord
   belongs_to :pilot, counter_cache: true
+  validates :startplace, :stopplace, :date :presence => true
 end
--- app/models/pilot.rb
+++ app/models/pilot.rb
@@ -1,5 +1,6 @@
 class Pilot < ApplicationRecord
   has_many :flights, dependent: :destroy
-
+  validates :pseudo, :uniqueness => true
+  validates :password, :length => { :in => 6..20, :too_short => "doit faire au moins %{count} caractères"}
   has_secure_password
 end
